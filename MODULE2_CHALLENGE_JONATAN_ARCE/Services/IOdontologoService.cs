ff67443284a29b8df55645288d7a684dbaa84aa9
JAVARCE-1 <jaw.arce@gmail.com>
2025-05-03 23:58:30 -0500

    Añadir a la carpeta Services la clase OdontologoService y su interface IOdontologoService

---
+++ MODULE2_CHALLENGE_JONATAN_ARCE/Services/IOdontologoService.cs
@@ -0,0 +1,18 @@
+﻿using MODULE2_CHALLENGE_JONATAN_ARCE.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODULE2_CHALLENGE_JONATAN_ARCE.Services
+{
+    public interface IOdontologoService
+    {
+        Odontologo GetCreateDentist(Odontologo odontologo);
+        Odontologo GetDentistById(int id);
+        List<Odontologo> GetDentists();
+        Odontologo GetDentistByNumberCOP(string numeroCop);
+        List<Cita> GetQuotesForDentist(int odontologoId);
+    }
+}
---
+++ MODULE2_CHALLENGE_JONATAN_ARCE/Services/OdontologoService.cs
@@ -0,0 +1,68 @@
+﻿using MODULE2_CHALLENGE_JONATAN_ARCE.Enums;
+using MODULE2_CHALLENGE_JONATAN_ARCE.Modelos;
+using MODULE2_CHALLENGE_JONATAN_ARCE.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODULE2_CHALLENGE_JONATAN_ARCE.Services
+{
+    public class OdontologoService: IOdontologoService
+    {
+        private readonly IOdontologoRepository _odontologoRepository;
+        private readonly ICitaRepository _citaRepository;
+        private readonly IPacienteRepository _pacienteRepository;
+
+        public OdontologoService(IOdontologoRepository odontologoRepository,
+            IPacienteRepository pacienteRepository,
+            ICitaRepository citaRepository)
+        {
+            this._odontologoRepository = odontologoRepository;
+            this._pacienteRepository = pacienteRepository;
+            this._citaRepository = citaRepository;
+        }
+
+        public Odontologo GetCreateDentist(Odontologo odontologo)
+        {
+            var newOdontologo = _odontologoRepository.GetByNumeroCop(odontologo.numeroCOP);
+            if (newOdontologo != null )
+            {
+                newOdontologo = new Odontologo
+                {
+                    NombreCompleto = odontologo.NombreCompleto,
+                    Sexo = odontologo.Sexo,
+                    FechaNacimiento = odontologo.FechaNacimiento,
+                    Telefono = odontologo.Telefono,
+                    Email = odontologo.Email,
+                    Especialidad = odontologo.Especialidad,
+                    numeroCOP=odontologo.numeroCOP
+                };
+            }
+            return newOdontologo;
+        }
+
+        public Odontologo GetDentistById(int id)
+        {
+            return _odontologoRepository.GetById(id);
+        }
+
+        public Odontologo GetDentistByNumberCOP(string numeroCop)
+        {
+            return _odontologoRepository.GetByNumeroCop(numeroCop);
+        }
+
+        public List<Odontologo> GetDentists()
+        {
+            return _odontologoRepository.GetAll();
+        }
+
+        public List<Cita> GetQuotesForDentist(int odontologoId)
+        {
+            var lista = _citaRepository.GetByListQuotesForDentistID(odontologoId);
+            return lista;
+        }
+    }
+}
