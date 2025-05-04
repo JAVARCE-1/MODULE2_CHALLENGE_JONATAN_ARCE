caa8932e8dc2943e643ce26110d8581b39bf48bd
JAVARCE-1 <jaw.arce@gmail.com>
2025-05-03 21:40:24 -0500

    Añadir a la carpeta Repositories la clase OdontologoRepository y la interfaces IRepository , IOdontologoRepository

---
+++ MODULE2_CHALLENGE_JONATAN_ARCE/Repositories/IOdontologoRepository.cs
@@ -0,0 +1,17 @@
+﻿using MODULE2_CHALLENGE_JONATAN_ARCE.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODULE2_CHALLENGE_JONATAN_ARCE.Repositories
+{
+    public interface IOdontologoRepository : IRepository<Odontologo>
+    {
+        Odontologo GetByNumeroExpediente(string numeroCOP);
+
+    }
+
+ 
+}
---
+++ MODULE2_CHALLENGE_JONATAN_ARCE/Repositories/OndotologoRepository.cs
@@ -0,0 +1,49 @@
+﻿using MODULE2_CHALLENGE_JONATAN_ARCE.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODULE2_CHALLENGE_JONATAN_ARCE.Repositories
+{
+    public class OndotologoRepository : IOdontologoRepository
+    {
+        private List<Odontologo> _dentist = new List<Odontologo>();
+
+        public void Add(Odontologo entity)
+        {
+            _dentist.Add(entity);
+        }
+
+        public void Delete(Odontologo entity)
+        {
+            _dentist.Remove(entity);
+        }
+
+        public List<Odontologo> GetAll()
+        {
+            return _dentist;
+        }
+
+        public Odontologo GetById(int id)
+        {
+            return _dentist.FirstOrDefault(x => x.Id == id);
+        }
+
+        public Odontologo GetByNumeroExpediente(string numeroCOP)
+        {
+            return _dentist.FirstOrDefault(x => x.numeroCOP == numeroCOP);
+        }
+
+        public void Update(Odontologo entity)
+        {
+            var index = _dentist.FindIndex(x => x.Id == entity.Id);
+
+            if (index != -1)
+            {
+                _dentist[index] = entity;
+            }
+        }
+    }
+}
