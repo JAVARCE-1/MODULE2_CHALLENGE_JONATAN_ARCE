8b1be5f9ad2f79efc0f7dc12caf2a373a2f6cea3
JAVARCE-1 <jaw.arce@gmail.com>
2025-05-03 23:16:19 -0500

    Añadir a la carpeta Services la clase  PacienteService y su interface IPacienteService

---
+++ MODULE2_CHALLENGE_JONATAN_ARCE/Services/IPacienteService.cs
@@ -0,0 +1,18 @@
+﻿using MODULE2_CHALLENGE_JONATAN_ARCE.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODULE2_CHALLENGE_JONATAN_ARCE.Services
+{
+    public interface IPacienteService
+    {
+        Paciente GetCreatePatients(Paciente paciente);
+        Paciente GetPatientById(int id);
+        List<Paciente> GetPatients();
+        Paciente GetPatientByNumberExpedient(string numeroExpediente);
+        List<Cita> GetPatientQuotes(int citaId);
+    }
+}
---
+++ MODULE2_CHALLENGE_JONATAN_ARCE/Services/PacienteService.cs
@@ -0,0 +1,64 @@
+﻿using MODULE2_CHALLENGE_JONATAN_ARCE.Modelos;
+using MODULE2_CHALLENGE_JONATAN_ARCE.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MODULE2_CHALLENGE_JONATAN_ARCE.Services
+{
+    public class PacienteService : IPacienteService
+    {
+        private readonly IPacienteRepository _pacienteRepository;
+        private readonly ICitaRepository _citaRepository;
+
+        public PacienteService (IPacienteRepository pacienteRepository, ICitaRepository citaRepository)
+        {
+            this._pacienteRepository = pacienteRepository;
+            this._citaRepository = citaRepository;
+        }
+
+        public Paciente GetCreatePatients(Paciente paciente)
+        {
+            var newPaciente = _pacienteRepository.GetByNumeroExpediente(paciente.NumeroExpediente);
+            if (newPaciente == null)
+            {
+                newPaciente = new Paciente
+                {
+                    NombreCompleto = paciente.NombreCompleto,
+                    Sexo = paciente.Sexo,
+                    FechaNacimiento = paciente.FechaNacimiento,
+                    Telefono = paciente.Telefono,
+                    Email = paciente.Email,
+                    NumeroExpediente = paciente.NumeroExpediente
+                };
+                _pacienteRepository.Add(newPaciente);
+            }
+            return newPaciente;
+        }
+
+        public Paciente GetPatientById(int id)
+        {
+            return _pacienteRepository.GetById(id);
+        }
+
+        public Paciente GetPatientByNumberExpedient(string numeroExpediente)
+        {
+            return _pacienteRepository.GetByNumeroExpediente(numeroExpediente);
+        }
+
+        public List<Cita> GetPatientQuotes(int citaId)
+        {
+            var lista = _citaRepository.GetByListQuotesForPatientID(citaId);
+            return lista;
+        }
+
+        public List<Paciente> GetPatients()
+        {
+            return _pacienteRepository.GetAll();
+        }
+    }
+}
